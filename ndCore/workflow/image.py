aeea7a16312fc6967bd2cdfc7437164db673baff
sbchild <a15315862177@163.com>
2020-12-15 11:17:02 +0800

    modify Workflow

--- ndCore/workflow/image.py
+++ ndCore/workflow/image.py
@@ -14,3 +14,8 @@
 #      You should have received a copy of the GNU General Public License
 #      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+from ..Node import Node
+from ..NodeType import Image, ImageLayer
+
+image = Node()
+imageLayer = Node()
